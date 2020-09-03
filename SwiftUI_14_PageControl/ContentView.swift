db151cbe72f874d08f1bda5d5e3d6684c630420d
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-03 11:03:43 +0200

    [FEATURE][PAGE CONTROL] * Control if user want to jump the tutorial was implemented

--- SwiftUI_14_PageControl/ContentView.swift
+++ SwiftUI_14_PageControl/ContentView.swift
@@ -4,43 +4,66 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var currentPage = 0
-    
+    @State private var jumpTutorial = false
+
     var body: some View {
-        VStack {
-            
-            ZStack {
-                if self.currentPage == 0 {
-                    Page1()
-                } else if self.currentPage == 1 {
-                    Page2()
-                } else if self.currentPage == 2 {
-                    Page3()
+        return Group {
+            if jumpTutorial {
+                MainPage()
+            } else {
+                VStack {
+                    
+                    ZStack {
+                        if self.currentPage == 0 {
+                            Page1()
+                        } else if self.currentPage == 1 {
+                            Page2()
+                        } else if self.currentPage == 2 {
+                            Page3()
+                        }
+                    }
+                    
+                    PageControl(currentPage: self.currentPage)
+                    
+                    HStack {
+                        Button(action: {
+                            if self.currentPage != 0 {
+                                self.currentPage -= 1
+                            }
+                        }) {
+                            Image(systemName: "chevron.left.circle.fill")
+                                .foregroundColor(.blue)
+                                .font(.title)
+                        }
+                        Button(action: {
+                            if self.currentPage < 2 {
+                                self.currentPage += 1
+                            }
+                        }) {
+                            Image(systemName: "chevron.right.circle.fill")
+                                .foregroundColor(.blue)
+                                .font(.title)
+                        }
+                    }
+                    if self.currentPage == 2 {
+                        Button(action: {
+                            self.jumpTutorial = true
+                            UserDefaults.standard.set(self.jumpTutorial, forKey: "jumpTutorial")
+                        }) {
+                            Text("Entendido, Saltar")
+                                .font(.system(.title, design: .rounded))
+                        }
+                    }
                 }
+                .padding(.bottom)
+                .animation(.default)
             }
-            
-            PageControl(currentPage: self.currentPage)
-            
-            HStack {
-                Button(action: {
-                    if self.currentPage != 0 {
-                        self.currentPage -= 1
-                    }
-                }) {
-                    Image(systemName: "chevron.left.circle.fill")
-                        .foregroundColor(.blue)
-                        .font(.title)
-                }
-                Button(action: {
-                    if self.currentPage < 2 {
-                        self.currentPage += 1
-                    }
-                }) {
-                    Image(systemName: "chevron.right.circle.fill")
-                        .foregroundColor(.blue)
-                        .font(.title)
-                }
+        }.onAppear {
+            if UserDefaults.standard.bool(forKey: "jumpTutorial") {
+                self.jumpTutorial = true
             }
         }
+
     }
 }
 
